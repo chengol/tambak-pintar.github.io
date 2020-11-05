c74d63d6558653678ad2f120696abe2b5d435470
Syauqy <syauqyaziz@gmail.com>
2020-11-05 20:56:58 +0700

    coba build

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,10 +19,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
 
     const blogTemplate = path.resolve('src/templates/blog.js')
-    // Query for markdown nodes to use in creating pages.
-    // You can query for whatever data you want to create pages for e.g.
-    // products, portfolio items, landing pages, etc.
-    // Variables can be added as the second function parameter
 
     const res = await graphql(
         `query{
